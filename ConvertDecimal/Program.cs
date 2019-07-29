0a44128c052a8ae04ffb662c1831def6f8304ab2
catzeromeio <catzeromeio@hotmail.com>
2019-07-30 00:41:01 +0800

    convert decimal & exceltitle

---
+++ ConvertDecimal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConvertDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Result = new List<int>();
+
+            Helper(Result, 200, 8);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        static void Helper(List<int> store, int decimalValue, int n)
+        {
+            int mod = decimalValue % n;
+            int div = decimalValue / n;
+
+            if (div != 0)
+                Helper(store, div, n);
+
+            store.Add(mod);
+        }
+
+    }
+}
--- ExcelTitle/Program.cs
+++ ExcelTitle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExcelTitle
 {
@@ -6,7 +7,42 @@ namespace ExcelTitle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ConvertToTitle(701);
+        }
+
+        static public string ConvertToTitle(int n)
+        {
+           
+            List<int> result = new List<int>();
+            Helper(result, n, 26);
+
+            List<char> final = new List<char>();
+            foreach (var item in result)
+                final.Add((char)(item + 64));
+
+            return new string(final.ToArray());
+        }
+
+        //10进制转换到26进制
+
+        static void Helper(List<int> store, int decimalValue, int n)
+        {
+            int mod = decimalValue % n;
+            int div = decimalValue / n;
+                
+            if (mod == 0)
+                div--;
+
+            if (div != 0)
+            {
+                Helper(store, div, n);
+            }
+
+            if (mod == 0)
+                store.Add(n);
+            else
+                store.Add(mod);
         }
     }
 }
